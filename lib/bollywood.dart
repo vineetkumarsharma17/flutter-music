d56bcdc75509deecd7f1d7d0d83079d2a02534a9
VINEET KUMAR SHARMA <vineetkumarsharma17@gmail.com>
2021-08-12 03:06:54 +0530

    Audio added commit 2

--- lib/bollywood.dart
+++ lib/bollywood.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 class Bollywood extends StatelessWidget {
-  final List song=[
-    'a',
-    'b'
+  final List songName=[
+    'Yara Teri Yari Ko',
+    'Apna Har Din'
   ];
+  final songs=[
+    "assets/audio/apna har din.mp3",
+    "assets/audio/yara teri yari.mp3"
+  ];
+
+final ap=AssetsAudioPlayer();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Bollywood Songs'),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.stop),
+        onPressed: (){
+          ap.pause();
+        },
+      ),
       body: ListView.builder(
-        itemCount: song.length,
+        itemCount: songName.length,
           itemBuilder:(context,index){
-          return ListTile(
-            title: Text(song[index]),
+          return Container(
+            //width: double.infinity,
+            decoration:BoxDecoration(
+              border: Border.all(color: Colors.black12)),
+            child: ListTile(
+              title: Text(songName[index]),
+              onTap: (){
+                ap.stop();
+                ap.open(Audio(songs[index]));
+                ap.play();
+                print(songs[index]);
+                print("Clicked");
+              },
+            ),
           );
       }),
     );
